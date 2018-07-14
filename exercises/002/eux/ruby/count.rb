110887d9006516dfa3e530afbcac1681cbf1d5e5
eux <euxuuu@gmail.com>
2018-07-14 21:56:37 +0800

    Fix exercise 2 from eux

--- exercises/002/eux/ruby/count.rb
+++ exercises/002/eux/ruby/count.rb
@@ -1,3 +1,3 @@
 puts 'What is the input string?'
-puts 'Must enter something!' while (input = gets.chomp).empty? == 0
+puts 'Must enter something!' while (input = gets.chomp).empty?
 puts "#{input} has #{input.size} characters."
