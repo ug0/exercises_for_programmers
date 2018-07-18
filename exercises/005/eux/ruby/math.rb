b6c75824835df6f36da97821d477799ef615fde2
eux <euxuuu@gmail.com>
2018-07-18 22:19:32 +0800

    Exercise 5 from eux

---
+++ exercises/005/eux/ruby/math.rb
@@ -0,0 +1,28 @@
+def valid_number
+  until number = Float(gets.chomp) rescue false && number >= 0
+    print "Must enter a positive number! "
+  end
+  number
+end
+
+def sum(a, b)
+  a + b
+end
+
+def difference(a, b)
+  a - b
+end
+
+def product(a, b)
+  a * b
+end
+
+def quotient(a, b)
+  a / b
+end
+
+print "What is the first number? "
+x = valid_number
+print "What is the second number? "
+y = valid_number
+print "#{x} + #{y} = #{sum(x, y)} \n#{x} - #{y} = #{difference(x, y)} \n#{x} * #{y} = #{product(x, y)} \n#{x} / #{y} = #{quotient(x, y)}"
