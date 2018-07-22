17eebf547de8284c094997a86920864d31184393
eux <euxuuu@gmail.com>
2018-07-22 22:06:31 +0800

    Update exercise 9 to support round room

--- exercises/009/eux/ruby/paint.rb
+++ exercises/009/eux/ruby/paint.rb
@@ -6,12 +6,27 @@ def valid_length(side)
   number
 end
 
+def valid_room_type
+  print "What is the type of the room, `round` or `rectangle`? "
+  until (type = gets.chomp.downcase) == 'round' || type == 'rectangle'
+    print "Only support room type: `round` or  `rectangle`. "
+  end
+  type
+end
+
 RATE = 350
 
-length = valid_length('length')
-width = valid_length('width')
+room_type = valid_room_type
+if room_type == 'round'
+  diameter = valid_length('diameter')
+  area_feet = Math::PI * (diameter / 2)**2
+else
+  length = valid_length('length')
+  width = valid_length('width')
+  area_feet = length * width
+end
 
-area_feet = length * width
 gallons_number = (area_feet / RATE).ceil
 
+puts "Your room type is #{room_type}."
 puts "You will need to purchase #{gallons_number} gallons of paint to cover #{area_feet.round(3)} square feet."
