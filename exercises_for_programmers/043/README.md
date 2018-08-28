06f3e359fd0c0b3a7e73752cb36fcbe70a7f494d
eux <euxuuu@gmail.com>
2018-08-28 22:48:53 +0800

    Exercise 43 from eux

---
+++ exercises_for_programmers/043/README.md
@@ -0,0 +1,27 @@
+# Exercise 43. Website Generator
+
+Programming languages can create files and folders too.
+Create a program that generates a website skeleton with the following specifications:
+
+- Prompt for the name of the site.
+- Prompt for the author of the site.
+- Ask if the user wants a folder for JavaScript files.
+- Ask if the user wants a folder for CSS files.
+- Generate an index.html file that contains the name of the
+site inside the <title> tag and the author in a <meta> tag.
+
+## Example Output
+
+Site name: awesomeco
+Author: Max Power
+Do you want a folder for JavaScript? y
+Do you want a folder for CSS? y
+Created ./awesomeco
+Created ./awesomeco/index.html
+Created ./awesomeco/js/
+Created ./awesomeco/css/
+
+## Challenges
+
+- Implement this in a scripting language on Windows, OSX, and Linux.
+- Implement this as a web application that provides the specified site as a zip file.
---
+++ exercises_for_programmers/043/eux/ruby/website_generator.rb
@@ -0,0 +1,51 @@
+def valid_input(name)
+  while (input = gets.chomp).empty?
+    print "#{name}: "
+  end
+  input
+end
+
+def valid_answer(name)
+  until ['yes', 'y', 'no', 'n'].include?(input = gets.chomp.downcase)
+    print "Do you want a folder for #{name} "
+  end
+  input == 'yes' || 'y'
+end
+
+print 'Site name: '
+site_name = valid_input('Site name')
+print 'Author: '
+author_name = valid_input('Author')
+print 'Do you want a folder for JavaScript? '
+want_js = valid_answer('JavaScript')
+print 'Do you want a folder for CSS? '
+want_css = valid_answer('CSS')
+
+html = <<~HTML
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta name="author" content="#{author_name}">
+      <title>#{author_name}</title>
+    </head>
+    <body>
+      <p>Hell, world</p>
+    </body>
+  </html>
+HTML
+
+site_path = "./#{site_name}"
+paths = [site_path]
+paths << "#{site_path}/js/" if want_js
+paths << "#{site_path}/css/" if want_css
+
+paths.each do |path|
+  Dir.mkdir path
+  puts "Created #{path}"
+end
+
+file_path = "#{site_path}/inex.html"
+File.open(file_path, 'w') do |f|
+  f << html
+end
+puts "Created #{file_path}"
